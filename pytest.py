7cd6ae20999eb4014dacee99383907b138f6ee8c
root <millergrnt@gmail.com>
2018-02-14 16:21:36 -0500

    Testing

--- pytest.py
+++ pytest.py
@@ -10,7 +10,7 @@ def func(x):
 	return x + 1;
 
 def main():
-	print("Beginning Tests:\n\tTesting func(3) == 4:"):
+	print("Beginning Tests:\n\tTesting func(3) == 4:");
 	if(func(3) == 4):
 		print("\t\t[+]Testing Successful");
 		sys.exit(0);
