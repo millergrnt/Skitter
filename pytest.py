37e1418bddfb94f9ab574b57e5f3165081f6ddda
root <millergrnt@gmail.com>
2018-02-14 14:16:54 -0500

    Added in testing environment, setup with Python. The test script is pytest.py

---
+++ pytest.py
@@ -0,0 +1,14 @@
+#
+#   Python Testing Script for Skitter
+#
+#   Authors: Grant Miller <gem1086@g.rit.edu>
+#	     Nick Querci <ncq7286@g.rit.edu>
+#
+import os
+
+
+def main():
+	sys.exit(0);
+
+if __name__ == '__main__':
+	main();
