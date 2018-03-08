f12097dd3b9f8c4b518c7fef68efd8e8916c7ced
root <millergrnt@gmail.com>
2018-03-08 17:21:14 -0500

    Finished side board, and banner

--- public-html/js/home.js
+++ public-html/js/home.js
@@ -1,15 +1,9 @@
 $('#likeButton').on({
 	'click' : function(){
-		if($(this).attr("src") == "img/like.svg"){
-			$(this).attr("src", "img/hearts.svg");
+		if($(this).children("img").attr("src") == "img/like.svg"){
+			$(this).children("img").attr("src", "img/hearts.svg");
 		} else {
-			$(this).attr("src", "img/like.svg");
+			$(this).children("img").attr("src", "img/like.svg");
 		}
 	}
-});
-
-$('#likeButton').hover(function(){
-	$(this).attr("src", "img/hearts.svg");
-}, function(){
-	$(this).attr("src", "img/like.svg");
 });
