3ffab4ed8e66cf343b6142b3d9701403ead2316d
root <millergrnt@gmail.com>
2018-03-14 15:06:29 -0400

    Periodic commit, still working on settings

--- public-html/php/settings.php
+++ public-html/php/settings.php
@@ -4,6 +4,7 @@
 	$dbpass = 'root';
 	$dbname = 'Skitter';
 	$conn = new mysqli($servername, $dbuname, $dbpass, $dbname);
+	$userid = NULL;
 
 	if($conn->connect_error){
 		die("Connection failed sorry");
@@ -13,61 +14,87 @@
 	$email = $_POST['email'];
 	$file = $_POST['fileToUpload'];
 
+	//Let's validate the post parameters, they will be NULL if they were not entered.
 	$username = validateUsername($username);
 	$email = validateEmail($email);
 	$file = validateFile($file);
 
-	//Let us assume the email is invalid, guilty until proved innocent
-	$emailValid = false;
+	//Let's check if the username is already taken
+	if(isset($username)){
+		$stmt = $conn->prepare("SELECT userid FROM Users WHERE username = ?;");
+		$stmt->bind_param("s", $username);
+
+		if(!$stmt->execute()){
+			print "Error in executing command";
+		}
+
+		$stmt->bind_result($userid);
+
+		if(!isset($userid)){
+			die "Error settings username: username already being used<br>";
+		}
+	}
+
+	//Get the user's userid based on their id cookie
+	$stmt->prepare("SELECT userid FROM Users WHERE idcookie = ?;");
+	$stmt->bind_param("s", $_COOKIE["id"]);
+
+	if(!$stmt->execute()){
+		print "Error in executing command";
+	}
+
+	$stmt->bind_result($userid);
+
+	if(isset($userid)){
+		print "Fatal Cookie Error<br>";
+	}
+
+	$id = $_COOKIE["id"];
+	$stmt = $conn->prepare("UPDATE Users SET username = ? WHERE idcookie = ?;");
+	$stmt->bind_param("ss", $username, $id);
+
+	if(!$stmt->execute()){
+		print "Error in executing command";
+	}
 
 	function validateUsername($unameUnsanitized){
 		$unameSanitized = strip_tags($unameUnsanitized);
-		if(strlen($unameSanitized) < 1){
-			print "Not doing anything with current username<br>";
-		} else {
-			print $unameSanitized . "<br>";
+		if(strlen($unameSanitized) >= 1){
+			return $unameSanitized;
 		}
+		$unameSanitized = NULL;
+		return $unameSanitized;
 	}
 
 	function validateEmail($emailUnsanitized){
 		$emailSanitized = strip_tags($emailUnsanitized);
-		if(strlen($emailSanitized) < 1){
-			print "Not doing anything with current email<br>";
-		} else {
-
-			if(checkdnsrr($emailSanitized, 'MX')){
-				$emailValid = true;
-				print "Email is valid: " . $emailSanitized . "<br";
+		if(strlen($emailSanitized) >= 1){
+			if(filter_var($emailSanitized, FILTER_VALIDATE_EMAIL)){
+				return $emailSanitized;
+			} else {
+				die("<br>Email is invalid<br>");
 			}
-			print $emailSanitized . "<br>";
 		}
+
+		$emailSanitized = NULL;
+		return $fileSanitized;
 	}
 
 	function validateFile($fileUnsanitized){
 		$fileSanitized = strip_tags($fileUnsanitized);
-		if(strlen($fileSanitized) < 1){
-			print "Not doing anything with current profile pic<br>";
-		} else {
+		if(strlen($fileSanitized) >= 1){
 			$image = getimagesize($fileSanitized) ? true : false;
 
 			if($image == true){
-				print $fileSanitized . "<br>";
+				return $fileSanitized;
 			} else {
-				print "<br>File uploaded was not an image<br>";
+				die("<br>File uploaded was not an image<br>");
 			}
 		}
+
+		$fileSanitized = NULL;
+		return $fileSanitized;
 	}
-
-	$stmt = $conn->prepare("SELECT * FROM Users WHERE username = (?);");
-	$stmt->bind_param("s", $username);
-
-	$result = mysql_query($stmt);
-
-	if(mysql_fetch_array($result) !== false)
-		die('Username alread taken');
-
-
-
 ?>
 <!DOCTYPE html>
 <html>
